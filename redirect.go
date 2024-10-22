d4e32d30bc853c9e6c90b11950cf8d799238b422
jackspirou <jack.spirou@me.com>
2024-10-22 00:59:19 -0400

    simplify redirect

--- redirect.go
+++ redirect.go
@@ -3,48 +3,23 @@ package egothic
 import (
 	"fmt"
 	"net/http"
-	"regexp"
-	"strings"
-	"text/template"
 
 	"github.com/labstack/echo/v4"
 )
 
-// redirectHTMLTemplate is the HTML template for the redirect page.
-const redirectHTMLTemplate = `
-<html>
-	<head>
-		<meta http-equiv="refresh" content="0;url=%s">
-		<script type="text/javascript">
-			window.location.href = "%s";
-		</script>
-	</head>
-	<body>
-		<p>If you are not redirected automatically, please <a href="%s">click here</a>.</p>
-	</body>
-</html>
-`
-
-// optimizedRedirectHTMLTemplate is the optimized HTML template for the redirect page.
-var optimizedRedirectHTMLTemplate = optimizeTemplate(redirectHTMLTemplate)
-
-// optimizeTemplate optimizes the HTML template.
-func optimizeTemplate(template string) string {
-	// Remove all newlines and tabs
-	optimized := strings.ReplaceAll(template, "\n", "")
-	optimized = strings.ReplaceAll(optimized, "\t", "")
-	// Remove extra spaces
-	optimized = regexp.MustCompile(`\s+`).ReplaceAllString(optimized, " ")
-	// Remove spaces between tags
-	optimized = regexp.MustCompile(`>\s+<`).ReplaceAllString(optimized, "><")
-	return strings.TrimSpace(optimized)
-}
+// redirectHTMLTemplate is the pre-optimized HTML template for the redirect page.
+const redirectHTMLTemplate = `<html><head><meta http-equiv="refresh" content="0;url=%s"><script type="text/javascript">window.location.href="%s";</script></head><body><p>If you are not redirected automatically, please <a href="%s">click here</a>.</p></body></html>`
 
 // Redirect redirects the user to the given URL.
 // This method attempts to avoid browser caching by setting appropriate headers.
 // It attempts a server-side redirect first, and if that fails, it sends a page with JavaScript redirect.
 func Redirect(e echo.Context, url string, opts ...Options) error {
 	config := newConfig(opts...)
+
+	if url == "" {
+		config.log("Error: Empty URL provided for redirect")
+		return fmt.Errorf("empty URL provided for redirect")
+	}
 
 	// Set headers to prevent caching
 	e.Response().Header().Set("Cache-Control", "no-store, no-cache, must-revalidate, max-age=0")
@@ -60,9 +35,7 @@ func Redirect(e echo.Context, url string, opts ...Options) error {
 	config.log("Server-side redirect to '" + url + "' failed")
 
 	// If server-side redirect fails, send a page with JavaScript redirect
-	e.Response().Header().Set("Content-Type", "text/html; charset=utf-8")
-	escapedURL := template.HTMLEscapeString(url)
-	html := fmt.Sprintf(optimizedRedirectHTMLTemplate, escapedURL, escapedURL, escapedURL)
 	config.log("Sending JavaScript redirect to '" + url + "'")
+	html := fmt.Sprintf(redirectHTMLTemplate, url, url, url)
 	return e.HTML(http.StatusOK, html)
 }
